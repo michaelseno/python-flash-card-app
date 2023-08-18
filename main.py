06ae43ac0bcaca7f8b1f5f85fbc1e6730b0bd047
Michael Jordan Seno <mjseno@Michaels-MacBook-Pro-2.local>
2023-08-18 21:19:58 +0800

    enhance code for readability

--- main.py
+++ main.py
@@ -8,23 +8,11 @@ df = pd.read_csv("data/french_words.csv")
 data_dict = df.to_dict(orient="records")
 
 
-def set_data():
-    rand_data = get_random_data()
+def next_card():
+    rand_data = random.choice(data_dict)
     canvas.itemconfig(language, text=list(rand_data.keys())[0])
     canvas.itemconfig(translated_text, text=rand_data[list(rand_data.keys())[0]])
 
-
-def get_random_data():
-    print(type(data_dict))
-    print(len(data_dict))
-    chosen = random.choice(data_dict)
-    print(chosen)
-    print(list(chosen.keys())[0])
-    print(chosen[list(chosen.keys())[0]])
-    return chosen
-
-
-initial = get_random_data()
 
 window = Tk()
 window.title("Flashy")
@@ -35,16 +23,18 @@ card_front_img = PhotoImage(file="./images/card_front.png")
 card_back_img = PhotoImage(file="./images/card_back.png")
 
 canvas.create_image(400, 263, image=card_front_img)
-language = canvas.create_text(400, 150, text=list(initial.keys())[0], fill="black", font=("Arial", 40, "italic"))
-translated_text = canvas.create_text(400, 263, text=initial[list(initial.keys())[0]], fill="black", font=("Arial", 60, "bold"))
+language = canvas.create_text(400, 150, text="Title", fill="black", font=("Arial", 40, "italic"))
+translated_text = canvas.create_text(400, 263, text="Word", fill="black", font=("Arial", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 
 x_button_img = PhotoImage(file="./images/wrong.png")
 v_button_img = PhotoImage(file="./images/right.png")
 
-right_button = Button(image=v_button_img, borderwidth=0, highlightthickness=0, command=set_data)
+right_button = Button(image=v_button_img, borderwidth=0, highlightthickness=0, command=next_card)
 right_button.grid(row=1, column=1)
 
 wrong_button = Button(image=x_button_img, borderwidth=0, highlightthickness=0)
 wrong_button.grid(row=1, column=0)
+
+next_card()
 window.mainloop()
